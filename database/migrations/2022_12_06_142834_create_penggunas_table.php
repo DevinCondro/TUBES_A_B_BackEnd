41e839b50a53b77d3d7f80b56f49142f5150a2da
Devin <devincondro123@gmail.com>
2022-12-10 14:01:56 +0700

    first commit

---
+++ database/migrations/2022_12_06_142834_create_penggunas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penggunas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('noTelp');
+            $table->string('tanggal_lahir');
+            $table->string('gender');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penggunas');
+    }
+};
